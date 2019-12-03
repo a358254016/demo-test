56812700f3aaafa4f9b51215fb991d95d3a9938a
zzy <358254016@qq.com>
2019-12-03 11:53:37 +0800

    add RoleController

---
+++ src/main/java/com/example/demo/contorller/RoleController.java
@@ -0,0 +1,15 @@
+package com.example.demo.contorller;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+/**
+ * @program: demo-test
+ * @description: 角色模块
+ * @author: Mr.Wang
+ * @create: 2019-12-03 11:51
+ **/
+public class RoleController {
+    public static void main(String[] args) {
+        System.out.println("逗你玩的");
+    }
+}
