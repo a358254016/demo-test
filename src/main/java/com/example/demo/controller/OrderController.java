9da66f920c8eac1f19bc010024f2345cfb309059
zzy <358254016@qq.com>
2020-01-12 22:21:32 +0800

    test2

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -12,6 +12,8 @@ import io.swagger.annotations.Api;
 public class OrderController {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println(2);
+        System.out.println(3);
         System.out.println("test2");
 
     }
