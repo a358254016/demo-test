6f94b64446aa99d4dce0e4dd5a2f1136ba68b667
zzy <358254016@qq.com>
2020-05-16 10:29:15 +0800

    fix_1

--- src/main/java/com/example/demo/controller/ShopController.java
+++ src/main/java/com/example/demo/controller/ShopController.java
@@ -13,6 +13,8 @@ public class ShopController {
         System.out.println(3);
         System.out.println(4);
         System.out.println(5);
-        System.out.println(6);
+        System.out.println(7);
+        System.out.println(7);
+        System.out.println(7);
     }
 }
